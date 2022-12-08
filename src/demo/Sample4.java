926dc3ff8b65a17d8ca8903298e62bfad3f7cf34
Komal Auti <Komal Auti@DESKTOP-SUUIO4D>
2022-12-08 10:30:20 +0530

    [komal auti]:Demo1

---
+++ src/demo/Sample4.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Sample4 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hi hello");
+		
+	}
+
+}
---
+++ src/demo/Sample5.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Sample5 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("pull the repository");
+		
+	}
+
+}
